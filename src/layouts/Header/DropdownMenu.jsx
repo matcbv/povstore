e0461c17591bc0d2e2bd8536a6b540549aa6689a
matcbv <matheuscbv23@gmail.com.br>
2025-03-06 19:04:13 -0300

    Adicao da logica para aparicao do submenu

--- src/layouts/Header/DropdownMenu.jsx
+++ src/layouts/Header/DropdownMenu.jsx
@@ -3,24 +3,19 @@ import { Submenu } from "./Submenu";
 export function DropdownMenu(){
 
     const showDropdownMenu = (e) => {
-        const element = e.target;
         const submenu = document.querySelector('.submenu');
-        const gendersList = document.querySelectorAll('.gender');
-        gendersList.forEach(item => {
-            console.log(element, item)
-            if(element === item){
-                console.log('opa')
-                submenu.classList.add('showSubmenu');
-            };
-        });
+        const genderCategory = document.querySelectorAll('.genderCategory');
+        genderCategory.forEach(element => {
+            element === e.target ? submenu.classList.add('showSubmenu'): submenu.classList.remove('showSubmenu');
+        })
     };
 
     return (
-        <nav className="dropdownMenu absolute left-0 top-full w-full" onMouseOver={ showDropdownMenu }>
+        <nav className="dropdownMenu absolute left-0 top-full w-full bg-[linear-gradient(to_right,_transparent,_#00000082,_transparent)] backdrop-blur-lg border border-b-2 border-t-red-600 border-b-black" onMouseOver={ showDropdownMenu }>
             <div className="flex justify-center items-center w-full">
-                <ul className="flex flex-col gap-y-4 text-white border-b-2 border-t border-black border-t-red-600 bg-[linear-gradient(to_right,_transparent,_#00000082,_transparent)] backdrop-blur-lg py-7 text-xl">
+                <ul className="flex flex-col gap-y-4 text-white  py-7 text-xl">
                     <li className="flex relative">
-                        <div className="catalog-items gender">
+                        <div className="catalog-items genderCategory">
                             Masculino
                             <img src="/assets/images/catalog_right_arrow.png" alt="Seta para direita" />
                         </div>
