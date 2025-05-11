a04b75e6f6f65d2c8c013d7c71d277175c99fe8a
matcbv <matheuscbv23@gmail.com>
2025-05-11 19:51:10 -0300

    Adicionada verificacao para existencia do item na sacola

--- src/utils/addItemToBag.js
+++ src/utils/addItemToBag.js
@@ -1,12 +1,29 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "../database/firebase";
+import {
+	addDoc,
+	collection,
+	getDocs,
+	query,
+	updateDoc,
+	where,
+} from 'firebase/firestore';
+import { db } from '../database/firebase';
 
-export async function addItemToBag(uid, itemData){
-    try{
-        const itemRef = collection(db, 'users', uid, 'checkout');
-        await addDoc(itemRef, itemData);
-        return {success: true, error: null};
-    } catch(e){
-        return {success: false, error: e.message};
-    };
-};
+export async function addItemToBag(uid, itemData) {
+	try {
+		const collectionRef = collection(db, 'users', uid, 'checkout');
+		const q = query(
+			collectionRef,
+			where('productId', '==', itemData.productId),
+		);
+		const queryDocs = (await getDocs(q)).docs;
+		if (queryDocs.length > 0 && queryDocs[0].data().size === itemData.size) {
+			const itemSnap = queryDocs[0];
+			await updateDoc(itemSnap.ref, { quantity: itemSnap.data().quantity + 1 });
+		} else {
+			await addDoc(collectionRef, itemData);
+		}
+		return { success: true, error: null };
+	} catch (e) {
+		return { success: false, error: e.message };
+	}
+}
