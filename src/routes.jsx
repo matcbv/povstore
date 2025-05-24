521895649108e87fa79685ef5706e925b56defcf
matcbv <matheuscbv23@gmail.com>
2025-05-24 00:43:55 -0300

    Adicao do layout wrapper para nas rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 // Componentes
+import { BaseLayout } from './layouts/BaseLayout';
 import { Home } from './pages/Home';
 import { Bag } from './pages/Bag';
 import { Session } from './pages/Session';
@@ -16,40 +17,42 @@ import { EditAccount } from './pages/EditAccount';
 import { PaymentMethods } from './pages/PaymentMethods';
 import { Orders } from './pages/Orders';
 import { OrdersDetails } from './pages/OrderDetails';
+import { Dashboard } from './pages/Dashboard';
 
 // Providers
 import { ProductProvider } from './contexts/ProductProvider/index';
-import { Dashboard } from './pages/Dashboard';
 
 export function AppRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route
-				path="/catalog/:gender?/:category?/:subcategory?"
-				element={
-					<ProductProvider>
-						<ProductsCatalog />
-					</ProductProvider>
-				}
-			/>
-			<Route path="/catalog/product/:productId" element={<ProductPage />} />
-			<Route path="/bag" element={<Bag />} />
-			<Route path="/checkout" element={<Checkout />} />
-			<Route path="/session" element={<Session />} />
-			<Route path="/register" element={<Register />} />
-			{/* Definindo uma rota base para a conta do usuário a ser utilizada por suas rotas filhas */}
-			<Route path="/account" element={<Account />}>
-				{/* As rotas filhas devem possuir caminhos relativos */}
-				<Route path="dashboard" element={<Dashboard />} />
-				<Route path="edit" element={<EditAccount />} />
-				<Route path="add-product" element={<AddProduct />} />
-				<Route path="favorites" element={<Favorites />} />
-				<Route path="payment-methods" element={<PaymentMethods />} />
-				<Route path="orders" element={<Orders />} />
-				<Route path="orders/:orderId" element={<OrdersDetails />} />
+			<Route element={<BaseLayout />}>
+				<Route path="/" element={<Home />} />
+				<Route
+					path="/catalog/:gender?/:category?/:subcategory?"
+					element={
+						<ProductProvider>
+							<ProductsCatalog />
+						</ProductProvider>
+					}
+				/>
+				<Route path="/catalog/product/:productId" element={<ProductPage />} />
+				<Route path="/bag" element={<Bag />} />
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/session" element={<Session />} />
+				<Route path="/register" element={<Register />} />
+				{/* Definindo uma rota base para a conta do usuário a ser utilizada por suas rotas filhas */}
+				<Route path="/account" element={<Account />}>
+					{/* As rotas filhas devem possuir caminhos relativos */}
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="edit" element={<EditAccount />} />
+					<Route path="add-product" element={<AddProduct />} />
+					<Route path="favorites" element={<Favorites />} />
+					<Route path="payment-methods" element={<PaymentMethods />} />
+					<Route path="orders" element={<Orders />} />
+					<Route path="orders/:orderId" element={<OrdersDetails />} />
+				</Route>
+				<Route path="*" element={<NotFound />} />
 			</Route>
-			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
